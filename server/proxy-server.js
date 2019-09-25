f42b6986324e4f35b0e37451bff6587828697d8d
Dan Fox <fox.j.dan@gmail.com>
2019-09-25 15:20:34 -0700

    altered to work on EC2

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -4,18 +4,20 @@ const axios = require('axios');
 
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
+const serviceIP = '172.31.8.16';
 
-
-// app.use('/:id/', express.static('public'));
+app.use(express.static('public'));
 
 app.get('/index_bundle.js', (req, res) => {
   console.log('received request for bundle');
-  axios.get('http://localhost:3004/index_bundle.js')
+  axios.get('http://172.31.8.16:3004/index_bundle.js')
     .then((response) => {
       // response = JSON.parse(response);
-      console.log(Object.keys(response));
+      // console.log(Object.keys(response));
       // console.log(response.data)
       res.send(response.data);
     })
@@ -29,7 +31,7 @@ app.get('/index_bundle.js', (req, res) => {
 app.get('/api/menu/:id', (req, res) => {
   // console.log('received request for menu');
   let id = req.params.id;
-  axios.get(`http://localhost:3004/api/menu/${id}`)
+  axios.get(`http://172.31.8.16:3004/api/menu/${id}`)
     .then((response) => {
       res.send(response.data);
     })
